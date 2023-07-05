bac04d7058d4e413a93e9de0e2c5bf68d2f31a8e
Mario Rocha <111705910+mariorzk@users.noreply.github.com>
2023-07-05 11:40:53 -0300

    Adicionada linha de remoção do Wine e Formatação de títulos

--- install.sh
+++ install.sh
@@ -6,11 +6,11 @@ A lista de programas será carregada em breve, porém antes de prosseguir, reali
 pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY bash -c '
 
 apt remove --purge samba-common-bin -y
-echo "------------------------------------------ ATUALIZANDO OS PACOTES"
+echo "------------------------------------------ ATUALIZANDO OS PACOTES ------------------------------------------ "
 apt update
 dpkg --configure -a
-# ------------------------------------------
-echo "------------------------------------------ Instalando Virtuall Box"
+
+echo "------------------------------------------ Instalando Virtuall Box------------------------------------------ "
 
 virtualbox -y && wget https://download.virtualbox.org/virtualbox/6.1.38/Oracle_VM_VirtualBox_Extension_Pack-6.1.38.vbox-extpack && sudo VBoxManage extpack install --replace  Oracle_VM_VirtualBox_Extension_Pack-6.1.38.vbox-extpack -y
 
@@ -18,11 +18,14 @@ echo "------------------------------------------ Atualizando os navegadores Chro
 apt install google-chrome-stable -y firefox firefox-locale-pt
 
 echo "> ------------------------------------------ INSTALANDO VISUAL STUDIO CODE -------------------------------------------"
+
 echo "> ------------------------------------------   Instalando as dependências   -----------------------------------------"
 sudo apt install software-properties-common apt-transport-https wget -y
+
 echo "> ------------------------ Adicionando chaves de verificação da integridade dos pacotes ------------------------------"
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 apt update
+
 echo "> --------------------------------------- Adionando repositório ------------------------------------------------------"
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 apt update
@@ -32,7 +35,7 @@ echo "> ------------------------------------------ INSTALANDO PIP DO PYTHON ----
 apt update
 apt install python3 -y pip -y
 
-echo "------------------------------------------ ATUALIZANDO OS PACOTES"
+echo "------------------------------------------ ATUALIZANDO OS PACOTES ------------------------------------------ "
 apt update
 
 echo "> ------------------------------------------ INSTALANDO E CONFIGURANDO O EPOPTES ------------------------------------------ "
@@ -40,10 +43,13 @@ echo "> ------------------------------------------ INSTALANDO E CONFIGURANDO O E
 #nano /etc/default/epoptes-client
 #epoptes-client -c
 
-#echo "------------------------------------------ CRIANDO UM USUÁRIO ADMINISTRADOR PARA O SISTEMA"
+echo "> ------------------------------------------ REMOVENDO O WINE DO SISTEMA ------------------------------------------ "
+apt remove --purge wine
+
+#echo "------------------------------------------ CRIANDO UM USUÁRIO ADMINISTRADOR PARA O SISTEMA ------------------------------------------ "
 sudo adduser administrador
 
-echo "------------------------------------------ MODIFICANDO CONFIGURAÇÕES DE ENERGIA"
+echo "------------------------------------------ MODIFICANDO CONFIGURAÇÕES DE ENERGIA ------------------------------------------ "
 '
 
 xfce4-power-manager-settings
