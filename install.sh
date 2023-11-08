aa466785e40c8204b244a0668f5b0d139c987cd9
JosemirAndrade <114767293+JosemirAndrade@users.noreply.github.com>
2023-11-08 12:53:52 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ dpkg --configure -a
 
 echo "------------------------------------------ Instalando Virtuall Box------------------------------------------ "
 
-virtualbox -y && wget https://download.virtualbox.org/virtualbox/6.1.38/Oracle_VM_VirtualBox_Extension_Pack-6.1.38.vbox-extpack && sudo VBoxManage extpack install --replace  Oracle_VM_VirtualBox_Extension_Pack-6.1.38.vbox-extpack -y
+#virtualbox -y && wget https://download.virtualbox.org/virtualbox/6.1.38/Oracle_VM_VirtualBox_Extension_Pack-6.1.38.vbox-extpack && sudo VBoxManage extpack install --replace  Oracle_VM_VirtualBox_Extension_Pack-6.1.38.vbox-extpack -y
 
 echo "------------------------------------------ Atualizando os navegadores Chrome e Firefox"
 apt install google-chrome-stable -y firefox firefox-locale-pt
@@ -20,38 +20,52 @@ apt install google-chrome-stable -y firefox firefox-locale-pt
 echo "> ------------------------------------------ INSTALANDO VISUAL STUDIO CODE -------------------------------------------"
 
 echo "> ------------------------------------------   Instalando as dependências   -----------------------------------------"
-sudo apt install software-properties-common apt-transport-https wget -y
+#sudo apt install software-properties-common apt-transport-https wget -y
 
 echo "> ------------------------ Adicionando chaves de verificação da integridade dos pacotes ------------------------------"
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-apt update
+#wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+#apt update
 
 echo "> --------------------------------------- Adionando repositório ------------------------------------------------------"
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-apt update
-sudo apt install code -y
+#sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+#apt update
+#sudo apt install code -y
 
 echo "> ------------------------------------------ INSTALANDO PIP DO PYTHON ------------------------------------------"
-apt update
+#apt update
 apt install python3 -y pip -y
 
 echo "------------------------------------------ ATUALIZANDO OS PACOTES ------------------------------------------ "
-apt update
+#apt update
 
 echo "> ------------------------------------------ INSTALANDO E CONFIGURANDO O EPOPTES ------------------------------------------ "
-apt install epoptes-client -y
-nano /etc/default/epoptes-client
-epoptes-client -c
+#apt install epoptes-client -y
+#nano /etc/default/epoptes-client
+#epoptes-client -c
 
 echo "> ------------------------------------------ REMOVENDO O WINE DO SISTEMA ------------------------------------------ "
-apt remove --purge wine
+#apt remove --purge wine
 
 #echo "------------------------------------------ CRIANDO UM USUÁRIO ADMINISTRADOR PARA O SISTEMA ------------------------------------------ "
-sudo adduser administrador
+#sudo adduser administrador
 
 echo "------------------------------------------ MODIFICANDO CONFIGURAÇÕES DE ENERGIA ------------------------------------------ "
+
+
+echo "------------------------------------------ INSTALANDO ARDUINO ------------------------------------------ "
+wget https://downloads.arduino.cc/arduino-1.8.16-linux64.tar.xz -O arduino-version.tar.xz
+tar xf arduino-version.tar.xz
+cd arduino-version
+./install.sh
+arduino
+
+sudo add-apt-repository ppa:damien-moore/codeblocks-stable
+sudo apt-get update
+sudo apt-get install codeblocks
+
+
 '
 
-xfce4-power-manager-settings
-xfce4-screensaver-preferences
-users-admin
+#xfce4-power-manager-settings
+#xfce4-screensaver-preferences
+#users-admin
