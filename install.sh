20aabfc9a81f3500889bb3acebe2cc6237c58c55
Mario Rocha <111705910+mariorzk@users.noreply.github.com>
2024-02-08 14:24:40 -0300

    Atualização do script
    *Adicionado a chave de autenticação dos repositórios do Anydesk.
    
    *Melhorada a formatação do script
    
    *Adicionado ação de criação do usuário administrador
    
    *Removido erros ortográficos.

--- install.sh
+++ install.sh
@@ -1,58 +1,57 @@
 #!/bin/bash#!/bin/bash
 
 zenity --info --width=250 --height=110 --text "<b><span color=\'green\'>CENTRAL DE PROGRAMAS</span></b> 
-A lista de programas será carregada em breve, porém antes de prosseguir, realize login como <b>administrador do sistema</b>"
+A lista de programas será instalada em breve, mas antes de prosseguir, realize login como <b>administrador do sistema</b>"
 
 pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY bash -c '
 
-apt remove --purge samba-common-bin -y
-echo "------------------------------------------ ATUALIZANDO OS PACOTES ------------------------------------------ "
+echo "****** ATUALIZANDO OS PACOTES E REPARANDO ****** "
+wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
 apt update
+apt remove --purge samba-common-bin -y
 dpkg --configure -a
 
-#echo "------------------------------------------ Instalando Virtuall Box------------------------------------------ "
+#echo "****** INSTALANDO VIRTUALBOX ****** "
 
 #virtualbox -y && wget https://download.virtualbox.org/virtualbox/6.1.38/Oracle_VM_VirtualBox_Extension_Pack-6.1.38.vbox-extpack && sudo VBoxManage extpack install --replace  Oracle_VM_VirtualBox_Extension_Pack-6.1.38.vbox-extpack -y
 
-echo "------------------------------------------ Atualizando os navegadores Chrome e Firefox"
+echo "****** ATUALIZANDO CHROME E FIREFOX ****** "
+
 apt install google-chrome-stable -y firefox firefox-locale-pt
 
-echo "------------------------------------------ INSTALANDO VISUAL STUDIO CODE -------------------------------------------"
+echo "****** INSTALANDO VISUAL STUDIO CODE ******"
 
-echo "------------------------------------------   Instalando as dependências   -----------------------------------------"
 sudo apt install software-properties-common apt-transport-https wget -y
-
-echo "------------------------ Adicionando chaves de verificação da integridade dos pacotes ------------------------------"
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-apt update
-
-echo "--------------------------------------- Adionando repositório ------------------------------------------------------"
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 apt update
 sudo apt install code -y
 
-echo ">------------------------------------------ INSTALANDO PYTHON 3 E PIP ------------------------------------------"
+echo "****** INSTALANDO PYTHON 3 E PIP ******"
 #apt update
 #apt install python3 -y pip -y
 
-#echo "------------------------------------------ ATUALIZANDO OS PACOTES ------------------------------------------ "
+#echo "****** ATUALIZANDO OS PACOTES ****** "
 apt update
 
-echo "------------------------------------------ INSTALANDO E CONFIGURANDO O EPOPTES ------------------------------------------ "
+echo "****** INSTALANDO E CONFIGURANDO O EPOPTES ****** "
 apt install epoptes-client -y
 nano /etc/default/epoptes-client
 epoptes-client -c
 
-echo "------------------------------------------ REMOVENDO O WINE DO SISTEMA ------------------------------------------ "
-apt remove --purge wine
+echo "****** REMOVENDO O WINE DO SISTEMA ****** "
+apt remove --purge wine wine64
 
-#echo "------------------------------------------ CRIANDO UM USUÁRIO ADMINISTRADOR PARA O SISTEMA ------------------------------------------ "
-#sudo adduser administrador
+echo "****** CRIANDO UM USUÁRIO ADMINISTRADOR PARA O SISTEMA ****** "
+sudo adduser -m administrador
 
-#echo "------------------------------------------ MODIFICANDO CONFIGURAÇÕES DE ENERGIA ------------------------------------------ "
+echo "****** DEFININDO SENHA PARA O USUARIO ADMINISTRADOR ****** "
+passwd administrador
+
+#echo "****** MODIFICANDO CONFIGURAÇÕES DE ENERGIA ****** "
 
 
-#echo "------------------------------------------ INSTALANDO ARDUINO IDE ------------------------------------------ "
+#echo "****** INSTALANDO ARDUINO IDE ****** "
 #wget https://downloads.arduino.cc/arduino-1.8.16-linux64.tar.xz -O arduino-version.tar.xz
 #tar xf arduino-version.tar.xz
 #cd arduino-1.8.16
@@ -63,21 +62,19 @@ apt remove --purge wine
 #chmod -R 777 /root/arduino-*
 #mv /root/arduino-* /home/aluno/
 
-#echo "------------------------------------------ INSTALANDO CODEBLOCKS ------------------------------------------ "
+#echo "****** INSTALANDO CODEBLOCKS ****** "
 #sudo add-apt-repository ppa:damien-moore/codeblocks-stable
 #sudo apt-get update
 #sudo apt-get install codeblocks -y
 #codeblocks
 
-#echo "------------------------------------------ INSTALANDO PYCHARM ------------------------------------------ "
+#echo "****** INSTALANDO PYCHARM ****** "
 #sudo add-apt-repository -y ppa:viktor-krivak/pycharm
 #apt update
 #apt install pycharm -y
 
-
-echo "------------------------------------------ REMOVENDO O SCRIPT ------------------------------------------ "
+echo "****** REMOVENDO O SCRIPT ****** "
 rm /home/aluno/labtools
-
 '
 users-admin
 #xfce4-power-manager-settings
